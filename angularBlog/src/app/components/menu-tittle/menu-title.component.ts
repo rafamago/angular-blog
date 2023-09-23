582e3fcb4511e60194fb6e6a3070b00454b1cc73
Rafamago <rafamago279@gmail.com>
2023-09-23 12:23:09 -0300

    content created

--- angularBlog/src/app/components/menu-tittle/menu-title.component.ts
+++ angularBlog/src/app/components/menu-tittle/menu-title.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-title',
   templateUrl: './menu-title.component.html',
-  styleUrls: ['./menu-title.component.css']
+  styleUrls: ['./menu-title.component.css',
+  './menu-title.responsive.component.css']
 })
 export class MenuTitleComponent {
 
-}
+  }
+
---
+++ angularBlog/src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [ {
+  "id":"1",
+  "title": "Jornada para me tornar Dev FullStack",
+  "description": "Um pouco sobre o Bootcamp e tudo que estou aprendendo.",
+  "text": "Olá pessoal! Nesse blog vou falar um pouco de como está sendo minha jornada até aqui no bootcamp, eu já tinha conhecimento em back-end, mas meu contato com front começou no bootcamp, aprendi a criar sites, calculadora em JavaScript, agora aprendi a criar esse blog que aprimorei o que foi ensinado até agora e estou desenvolvendo ele aos poucos.",
+  "photoCover":"https://myways-public-data-prod.s3.ap-south-1.amazonaws.com/myways-resource-library/blogs/roadmap-to-become-a-full-stack-developer_Image_blogs.png"
+},
+{
+"id":"2",
+"title": "Aprendendo controle de versão com Git e Github",
+"description": "Um pouco sobre o Bootcamp e tudo que estou aprendendo.",
+"text":"Tive meu primeiro contato com controle de versão no bootcamp, mas com a prática desde o começo, já aprendi a fazer uso das funções e como o GitHub pode ajudar a melhorar.",
+"photoCover":"https://hermes.dio.me/articles/cover/febfe489-d027-4d8c-83a0-3da59310283b.png"
+},
+{
+"id":"3",
+"title": "Criando uma pokedéx com HTML, CSS e JavaScript",
+"description": "Um pouco do meu projeto Pokedéx.",
+"text":"Nesse projeto usei tanto as aulas do Bootcamp, como tutoriais de Pokedéx no Youtube para estilizar da forma que queria.",
+"photoCover":"https://meunegocio.uol.com.br/blog/wp-content/uploads/2020/07/html-css-ou-javascript-qual-aprender-primeiro-1499777555318_600x320.jpg"
+},
+{
+"id":"4",
+"title": "Criando meu primeiro blog com Angular",
+"description": "O desenvolvimento desse blog.",
+"text":"Esse blog é meu projeto do Bootcamp que fiz umas alterações e adicionais mais elementos para ter o formato que estou desenvolvendo.",
+"photoCover":"https://bairesdev.mo.cloudinary.net/blog/2023/06/the-seo-guide-to-angular.png?tx=w_3840,q_auto"
+}]
